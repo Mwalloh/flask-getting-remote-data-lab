1080e16290243e22049c1f1a41edea1b3ca5b598
Jim Ochieng <jimochieng64@gmail.com>
2026-03-13 03:17:37 +0300

    Requested data from a URL and converted response to JSON format

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,15 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        # Sends the request to the URL and gets the response
+        response = requests.get(self.url)
+        return response
 
     def load_json(self):
-        pass
+        # Converts response from 'get_response_body()' to JSON format
+        data = self.get_response_body().json()
+        return data
+
+
+results = GetRequester("https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json").load_json()
+print(json.dumps(results, indent=4))
